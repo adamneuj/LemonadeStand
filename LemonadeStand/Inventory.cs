7c9fc167341515ba9211977eaf624f94bedde0a4
Adam Neujahr <adamneujahr@gmail.com>
2019-10-01 11:57:00 -0500

    Added lists for sugar, cups, and icecubes in inventory.

--- LemonadeStand/Inventory.cs
+++ LemonadeStand/Inventory.cs
@@ -10,11 +10,17 @@ namespace LemonadeStand
     {
         //member methods
         public List<Lemon> lemons;
+        public List<Sugar> cupsOfSugar;
+        public List<IceCube> iceCubes;
+        public List<Cup> cups;
 
         //constructor
         public Inventory()
         {
             lemons = new List<Lemon>();
+            cupsOfSugar = new List<Sugar>();
+            iceCubes = new List<IceCube>();
+            cups = new List<Cup>();
         }
 
         //member variables
